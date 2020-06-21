f21336dc8d5373ba94f05711cc47b13ef7b00dcf
alec <alecnader@gmail.com>
2020-06-21 02:55:32 -0400

    Updating the HTTP router to gorilla/mux and adding the ability to query a person from the People[]

--- main.go
+++ main.go
@@ -1,41 +1,67 @@
 package main
 
 import (
-    "fmt"
-    "log"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+// Person is the JSON structure I would like to export
 type Person struct {
-    Name string `json:"Name"`
-    Occupation string `json:"Occupation"`
+	ID         string `json:"Id"`
+	Name       string `json:"Name"`
+	Occupation string `json:"Occupation"`
 }
 
+// People - the fake dataset I will use in place of a database.
 var People []Person
 
-func allPeople(w http.ResponseWriter, r *http.Request){
-    fmt.Println("GET all people")
-    json.NewEncoder(w).Encode(People)
+// AllPeople is a function which is called when the endpoint /allPeople is requested. AllPeople returns the encoded JSON of the People object.
+func allPeople(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET all people")
+	json.NewEncoder(w).Encode(People)
 }
 
-func home(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World!")
-    fmt.Println("home")
+// home function handles the homepage request and returns a string to the page.
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
 
+// returnPerson function utilizes the mux router and the query parameter to select and return the proper person by ID.
+func returnPerson(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	// _ can be used as a throw away variable for iteration. Iterate through the array of people and find the one that matches the query parameter ID.
+	for _, person := range People {
+		if person.ID == key {
+			json.NewEncoder(w).Encode(person)
+		}
+	}
+}
+
+// handleRequests handles HTTP requests by use of the gorilla/mux router instead of the http/net.
 func handleRequests() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/allPeople", allPeople)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", home)
+	myRouter.HandleFunc("/allPeople", allPeople)
+	myRouter.HandleFunc("/person/{id}", returnPerson)
+
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
 
 	People = []Person{
-        Person{Name: "Alec", Occupation: "Software Engineer"},
-		Person{Name: "Jack", Occupation: "Chef"},
-		Person{Name: "Tom",  Occupation: "Cashier"},
+		Person{ID: "1", Name: "Alec", Occupation: "Software Engineer"},
+		Person{ID: "2", Name: "Jack", Occupation: "Chef"},
+		Person{ID: "3", Name: "Tom", Occupation: "Cashier"},
 	}
-	
-    handleRequests()
+
+	handleRequests()
 }
